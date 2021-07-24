4b7432865aab63d04302c54ae9466c3af78afd43
Mark Lester Bolotaolo <mlab817@gmail.com>
2021-07-24 15:57:53 +0800

    fixed indentation

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from models.ltt import run_ols
 from models.cagr import run_cagr
 from models.arima import run_arima
 
-
 import pandas as pd
-
 
 app = Flask(__name__)
 
@@ -14,53 +12,55 @@ app = Flask(__name__)
 # A welcome message to test our server
 @app.route('/', methods=['GET', 'POST'])
 def index():
-     response = None
-     if request.method == 'GET':
-         response = render_template('upload.html')
-     #
-     if request.method == 'POST':
-         uploaded_file = request.files.get('file')
-         # validate the uploaded files if it contains 5 columns
+    response = None
+    if request.method == 'GET':
+        response = render_template('upload.html')
+    #
+    if request.method == 'POST':
+        uploaded_file = request.files.get('file')
+        # validate the uploaded files if it contains 5 columns
 
-         df = pd.read_csv(uploaded_file,
-                          thousands=',',
-                          header='infer', )
+        df = pd.read_csv(uploaded_file,
+                         thousands=',',
+                         header='infer', )
 
-         if len(df.axes[1]) != 5:
-             return 'File must have 5 columns: year, production, area harvested, yield, and per capita. Your file has %s' % str(len(df.axes[1]))
+        if len(df.axes[1]) != 5:
+            return 'File must have 5 columns: year, production, area harvested, yield, and per capita. Your file has %s' % str(
+                len(df.axes[1]))
 
-         prepared_data = prepare_data(df)
+        prepared_data = prepare_data(df)
 
-         ols_result = run_ols(data=prepared_data, x_var='yield', y_var='year', log=True)
+        ols_result = run_ols(data=prepared_data, x_var='yield', y_var='year', log=True)
 
-         if request.is_json:
-             response = jsonify(ols_result)
-         else:
-             response = ols_result
+        if request.is_json:
+            response = jsonify(ols_result)
+        else:
+            response = ols_result
 
-     return response # render_template('upload.html', shape=(32, 3))
+    return response  # render_template('upload.html', shape=(32, 3))
 
 
- # TODO: Make this prepare data dynamic
- def prepare_data(data=pd.DataFrame()):
-     """
-     Prepare uploaded data for processing by defining the correct data types
+# TODO: Make this prepare data dynamic
+def prepare_data(data=pd.DataFrame()):
+    """
+    Prepare uploaded data for processing by defining the correct data types
 
-     :param data:
-     :return: prepared data
-     """
-     data.columns = ["year", "production", "area_harvested", "yield", "per_capita"]
+    :param data:
+    :return: prepared data
+    """
+    data.columns = ["year", "production", "area_harvested", "yield", "per_capita"]
 
-     # convert data to float as sometimes the data is not of correct type (e.g. string instead of float)
-     data["production"] = pd.to_numeric(data["production"])
-     data["area_harvested"] = pd.to_numeric(data["area_harvested"])
-     data["yield"] = pd.to_numeric(data["yield"])
-     data["per_capita"] = pd.to_numeric(data["per_capita"])
+    # convert data to float as sometimes the data is not of correct type (e.g. string instead of float)
+    data["production"] = pd.to_numeric(data["production"])
+    data["area_harvested"] = pd.to_numeric(data["area_harvested"])
+    data["yield"] = pd.to_numeric(data["yield"])
+    data["per_capita"] = pd.to_numeric(data["per_capita"])
 
-     # verify data types
-     data.describe()
+    # verify data types
+    data.describe()
 
-     return data
+    return data
+
 
 if __name__ == '__main__':
     app.debug = True
